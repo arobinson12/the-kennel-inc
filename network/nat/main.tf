aa8d1ca94b9819a73afde53bf325c8095ad47c71
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2022-04-24 17:02:23 -0500

    Update main.tf

--- network/nat/main.tf
+++ network/nat/main.tf
@@ -11,5 +11,5 @@ resource "google_compute_router_nat" "nat" {
   region                             = "us-central1"
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-  project_id                         = var.project_id
+  project                         = var.project_id
 }
