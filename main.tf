85b0018d98f4961dd82978868929c1f5044a87b5
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2022-04-24 10:44:59 -0500

    Update and rename providers.tf to main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+#terraform {
+#  required_version = ">= 0.12"
+#}
+#provider "google" {
+#  credentials = file(var.gcp_auth_file)
+#  project     = var.gcp_project
+#  region      = var.gcp_region
+#}
+
+provider "google" {
+  credentials = file(var.gcp_auth_file)
+  project = "interstellar-14"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+module "network" {
+  source = "./network/vpc"
+}
