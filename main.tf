c12e531f8335f0d6356753005c1df4ecfec4b0eb
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2022-04-24 11:05:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,27 @@
-#terraform {
-#  required_version = ">= 0.12"
-#}
-#provider "google" {
-#  credentials = file(var.gcp_auth_file)
-#  project     = var.gcp_project
-#  region      = var.gcp_region
-#}
-
+terraform {
+  required_version = ">= 0.12"
+}
 provider "google" {
   credentials = file(var.gcp_auth_file)
-  project = "interstellar-14"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project     = var.gcp_project
+  region      = var.gcp_region
 }
 
-module "network" {
-  source = "./network/vpc"
-  
-  project_id = var.project_id
-  network_name = var.network_name
+#provider "google" {
+#  credentials = file(var.gcp_auth_file)
+#  project = "interstellar-14"
+#  region  = "us-central1"
+#  zone    = "us-central1-c"
+#}
+
+module "vpc" {
+  source                                 = "./network/vpc"
+  network_name                           = var.network_name
+  auto_create_subnetworks                = var.auto_create_subnetworks
+  routing_mode                           = var.routing_mode
+  project_id                             = var.project_id
+  description                            = var.description
+  shared_vpc_host                        = var.shared_vpc_host
+  delete_default_internet_gateway_routes = var.delete_default_internet_gateway_routes
+  mtu                                    = var.mtu
 }
