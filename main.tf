60eb1a42e5e6a313e27ea3393bc948884e536a33
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2022-04-24 11:52:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_version = ">= 0.12"
-}
-provider "google" {
-  credentials = file(var.gcp_auth_file)
-  project     = var.gcp_project
-  region      = var.gcp_region
-}
-
-#provider "google" {
-#  credentials = file(var.gcp_auth_file)
-#  project = "interstellar-14"
-#  region  = "us-central1"
-#  zone    = "us-central1-c"
-#}
-
 module "vpc" {
   source                                 = "./network/vpc"
   network_name                           = var.network_name
