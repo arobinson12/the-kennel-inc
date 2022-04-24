938e0df726246f23ff2d8230cc50bba2510e0e2a
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2022-04-24 11:56:55 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,15 @@
 terraform {
   required_version = ">= 0.12"
 }
-provider "google" {
-  credentials = file(var.gcp_auth_file)
-  project     = var.gcp_project
-  region      = var.gcp_region
-}
-
 #provider "google" {
 #  credentials = file(var.gcp_auth_file)
-#  project = "interstellar-14"
-#  region  = "us-central1"
-#  zone    = "us-central1-c"
+#  project     = var.gcp_project
+#  region      = var.gcp_region
 #}
+
+provider "google" {
+#  credentials = file(var.gcp_auth_file)
+  project = "interstellar-14"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
