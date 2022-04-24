470095c11ecde390e96685e13279b5ed311eaa07
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2022-04-24 17:13:45 -0500

    Update main.tf

--- network/nat/main.tf
+++ network/nat/main.tf
@@ -5,6 +5,10 @@ resource "google_compute_router" "router" {
   project = var.project_id
 }
 
+resource "time_sleep" "wait_3_seconds" {
+  create_duration = "3s"
+}
+
 resource "google_compute_router_nat" "nat" {
   name                               = "prod-nat"
   router                             = "cr-nat"
@@ -12,4 +16,5 @@ resource "google_compute_router_nat" "nat" {
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
   project                         = var.project_id
+  depends_on                         = [google_compute_router.router]
 }
