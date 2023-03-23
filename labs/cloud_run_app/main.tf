f86fe832e7cc771cf32d005b5ddf9cf61ff75111
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2023-03-23 10:16:33 -0500

    Update main.tf

--- labs/cloud_run_app/main.tf
+++ labs/cloud_run_app/main.tf
@@ -20,137 +20,137 @@ provider "google-beta" {
 
 # Granting this BU1 project access to the shared VPC
 
-resource "google_compute_shared_vpc_service_project" "shared_vpc_attachment" {
-  host_project      = var.shared_vpc_project_id
-  service_project   = var.project_id
-}
+# resource "google_compute_shared_vpc_service_project" "shared_vpc_attachment" {
+#   host_project      = var.shared_vpc_project_id
+#   service_project   = var.project_id
+# }
 
 
-# Service accounts needed for the app
+# # Service accounts needed for the app
 
-resource "google_project_iam_member" "elixir_app_identity_platform" {
-  project = var.project_id
-  role    = "roles/identityplatform.admin"
-  member  = "serviceAccount:${google_service_account.elixir_app.email}"
-}
+# resource "google_project_iam_member" "elixir_app_identity_platform" {
+#   project = var.project_id
+#   role    = "roles/identityplatform.admin"
+#   member  = "serviceAccount:${google_service_account.elixir_app.email}"
+# }
 
-resource "google_project_iam_member" "elixir_app_sdk_admin" {
-  project = var.project_id
-  role    = "roles/identityplatform.admin"
-  member  = "serviceAccount:${google_service_account.elixir_app.email}"
-}
-
-
-# Creating the cloud run app
-resource "google_cloud_run_service" "elixir_app" {
-  name     = "elixir-app"
-  location = var.region
-
-  template {
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale" : "10"
-      }
-    }
-
-    spec {
-      containers {
-        image = "gcr.io/${var.project_id}/sample-login-app-image:latest"
-      }
-      service_account_name = google_service_account.elixir_app.email
-    }
-  }
-}
+# resource "google_project_iam_member" "elixir_app_sdk_admin" {
+#   project = var.project_id
+#   role    = "roles/identityplatform.admin"
+#   member  = "serviceAccount:${google_service_account.elixir_app.email}"
+# }
 
 
-# Making the Cloud Run app available to public
-resource "google_cloud_run_service_iam_member" "public" {
-  provider = google-beta
+# # Creating the cloud run app
+# resource "google_cloud_run_service" "elixir_app" {
+#   name     = "elixir-app"
+#   location = var.region
 
-  service  = google_cloud_run_service.elixir_app.name
-  location = var.region
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
+#   template {
+#     metadata {
+#       annotations = {
+#         "autoscaling.knative.dev/maxScale" : "10"
+#       }
+#     }
 
-# Attaching SA to CR app
-resource "google_service_account" "elixir_app" {
-  account_id   = "elixir-app"
-  display_name = "Elixir App Service Account"
-}
+#     spec {
+#       containers {
+#         image = "gcr.io/${var.project_id}/sample-login-app-image:latest"
+#       }
+#       service_account_name = google_service_account.elixir_app.email
+#     }
+#   }
+# }
 
 
-# Identity Platform tenant and microsoft config. Need to add tenant id, client secret, and secret version resource name
+# # Making the Cloud Run app available to public
+# resource "google_cloud_run_service_iam_member" "public" {
+#   provider = google-beta
 
-#resource "google_identity_platform_tenant" "tenant" {
-#  display_name = "Elixir App Tenant"
-#  project      = var.project_id
-#}
+#   service  = google_cloud_run_service.elixir_app.name
+#   location = var.region
+#   role     = "roles/run.invoker"
+#   member   = "allUsers"
+# }
 
-#resource "google_identity_platform_tenant_oauth_idp_config" "microsoft_ad" {
-#  display_name  = "Microsoft AD"
-#  enabled       = true
-#  tenant        = google_identity_platform_tenant.tenant.name
-#  client_id     = "your_microsoft_ad_client_id"
-#  issuer        = "https://login.microsoftonline.com/your_tenant_id/v2.0"
-#  client_secret = {
-#    secret_manager_secret_version = "your_microsoft_ad_client_secret_version_resource_name"
-#  }
-#}
+# # Attaching SA to CR app
+# resource "google_service_account" "elixir_app" {
+#   account_id   = "elixir-app"
+#   display_name = "Elixir App Service Account"
+# }
 
-# Load Balancer. Need to add domain to SSL cert and URL map
 
-resource "google_compute_global_address" "load_balancer_ip" {
-  name          = "elixir-app-load-balancer-ip"
-  purpose       = "EXTERNAL"
-  address_type  = "EXTERNAL"
-}
+# # Identity Platform tenant and microsoft config. Need to add tenant id, client secret, and secret version resource name
 
-resource "google_compute_managed_ssl_certificate" "ssl_cert" {
-  name = "elixir-app-ssl-cert"
-  managed {
-    domains = ["elixir.example.com"]
-  }
-}
+# #resource "google_identity_platform_tenant" "tenant" {
+# #  display_name = "Elixir App Tenant"
+# #  project      = var.project_id
+# #}
 
-resource "google_compute_target_https_proxy" "https_proxy" {
-  name             = "elixir-app-https-proxy"
-  url_map          = google_compute_url_map.url_map.self_link
-  ssl_certificates = [google_compute_managed_ssl_certificate.ssl_cert.self_link]
-}
+# #resource "google_identity_platform_tenant_oauth_idp_config" "microsoft_ad" {
+# #  display_name  = "Microsoft AD"
+# #  enabled       = true
+# #  tenant        = google_identity_platform_tenant.tenant.name
+# #  client_id     = "your_microsoft_ad_client_id"
+# #  issuer        = "https://login.microsoftonline.com/your_tenant_id/v2.0"
+# #  client_secret = {
+# #    secret_manager_secret_version = "your_microsoft_ad_client_secret_version_resource_name"
+# #  }
+# #}
 
-resource "google_compute_forwarding_rule" "https" {
-  name        = "elixir-app-https-forwarding-rule"
-  ip_protocol = "TCP"
-  port_range  = "443"
+# # Load Balancer. Need to add domain to SSL cert and URL map
 
-  load_balancing_scheme = "EXTERNAL"
-  target          = google_compute_target_https_proxy.https_proxy.self_link
-  ip_address      = google_compute_global_address.load_balancer_ip.address
-}
+# resource "google_compute_global_address" "load_balancer_ip" {
+#   name          = "elixir-app-load-balancer-ip"
+#   purpose       = "EXTERNAL"
+#   address_type  = "EXTERNAL"
+# }
 
-resource "google_compute_backend_service" "backend" {
-  name        = "elixir-app-backend-service"
-  protocol    = "HTTP2"
-  timeout_sec = 10
+# resource "google_compute_managed_ssl_certificate" "ssl_cert" {
+#   name = "elixir-app-ssl-cert"
+#   managed {
+#     domains = ["elixir.example.com"]
+#   }
+# }
 
-  backend {
-    group = google_cloud_run_service.elixir_app.status[0].url
-  }
-}
+# resource "google_compute_target_https_proxy" "https_proxy" {
+#   name             = "elixir-app-https-proxy"
+#   url_map          = google_compute_url_map.url_map.self_link
+#   ssl_certificates = [google_compute_managed_ssl_certificate.ssl_cert.self_link]
+# }
 
-resource "google_compute_url_map" "url_map" {
-  name = "elixir-app-url-map"
+# resource "google_compute_forwarding_rule" "https" {
+#   name        = "elixir-app-https-forwarding-rule"
+#   ip_protocol = "TCP"
+#   port_range  = "443"
 
-  default_service = google_compute_backend_service.backend.self_link
+#   load_balancing_scheme = "EXTERNAL"
+#   target          = google_compute_target_https_proxy.https_proxy.self_link
+#   ip_address      = google_compute_global_address.load_balancer_ip.address
+# }
 
-  host_rule {
-    hosts        = ["elixir.example.com"]
-    path_matcher = "allpaths"
-  }
+# resource "google_compute_backend_service" "backend" {
+#   name        = "elixir-app-backend-service"
+#   protocol    = "HTTP2"
+#   timeout_sec = 10
 
-  path_matcher {
-    name = "allpaths"
-    default_service = google_compute_backend_service.backend.self_link
-  }
-}
+#   backend {
+#     group = google_cloud_run_service.elixir_app.status[0].url
+#   }
+# }
+
+# resource "google_compute_url_map" "url_map" {
+#   name = "elixir-app-url-map"
+
+#   default_service = google_compute_backend_service.backend.self_link
+
+#   host_rule {
+#     hosts        = ["elixir.example.com"]
+#     path_matcher = "allpaths"
+#   }
+
+#   path_matcher {
+#     name = "allpaths"
+#     default_service = google_compute_backend_service.backend.self_link
+#   }
+# }
