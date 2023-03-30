f7f470e26003ec037fe3fa274316aaf1a78495da
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2023-03-30 08:44:16 -0500

    Create gke-enable-posture-all.sh

---
+++ other/scripts/gke-enable-posture-all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Get all projects in the organization
+projects=$(gcloud projects list --format='value(project_id)')
+
+# Loop through each project
+for project in $projects; do
+  echo "Checking project $project..."
+
+  # Get all GKE clusters in the project
+  clusters=$(gcloud container clusters list --project="$project" --format='value(name, location)')
+
+  # Loop through each cluster
+  for cluster in $clusters; do
+    name=$(echo "$cluster" | awk '{print $1}')
+    region=$(echo "$cluster" | awk '{print $2}')
+
+    # Enable workload-vulnerability-scanning
+    echo "Enabling workload-vulnerability-scanning for cluster $name in region $region..."
+    gcloud beta container clusters update "$name" --region="$region" --enable-workload-vulnerability-scanning
+
+    # Wait for 30 seconds before enabling the next feature
+    sleep 30
+
+    # Enable workload-config-audit
+    echo "Enabling workload-config-audit for cluster $name in region $region..."
+    gcloud beta container clusters update "$name" --region="$region" --enable-workload-config-audit
+
+    # Wait for 30 seconds before processing the next cluster
+    sleep 30
+  done
+done
