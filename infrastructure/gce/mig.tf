ea28e57225164fc517e6e3a6c4c94ef0bd741d69
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2023-04-18 18:39:57 -0500

    Create mig.tf

---
+++ infrastructure/gce/mig.tf
@@ -0,0 +1,47 @@
+resource "google_compute_instance_template" "vm_template" {
+  name_prefix  = "bu1-prod-app-instance-template-"
+  project      = "bu1-prod-app"
+  machine_type = "e2-medium"
+
+  disk {
+    boot = true
+    auto_delete = true
+    source_image = "projects/debian-cloud/global/images/family/debian-10"
+  }
+
+  network_interface {
+    network = "projects/prd-shared-host/global/networks/vpc-prod-shared"
+    subnetwork = "projects/prd-shared-host/regions/us-central1/subnetworks/subnet-bu1-1"
+  }
+
+  service_account {
+    email  = "generic-web@bu1-prod-app.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  metadata_startup_script = <<-EOT
+    #! /bin/bash
+    apt-get update
+    apt-get install -y nginx
+    echo "Hello from $(hostname)" > /var/www/html/index.html
+    systemctl start nginx
+  EOT
+
+  labels = {
+    business_unit = "bu1"
+    approval_id   = "05012023"
+  }
+}
+
+resource "google_compute_instance_group_manager" "vm_instance_group" {
+  name        = "bu1-prod-app-instance-group"
+  project     = "bu1-prod-app"
+  base_instance_name = "bu1-prod-app-instance"
+  zone        = "us-central1-c"
+
+  version {
+    instance_template = google_compute_instance_template.vm_template.self_link
+  }
+
+  target_size = 2
+}
