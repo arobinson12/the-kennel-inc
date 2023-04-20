77394d9e31e7a6a2c6a615e7c4f61931249afde0
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2023-04-20 13:20:28 -0500

    Update tag-fw.tf

--- network/tags-firewall/tag-fw.tf
+++ network/tags-firewall/tag-fw.tf
@@ -71,6 +71,7 @@ resource "google_compute_instance" "proxy_1" {
 }
 
 resource "google_tags_tag_binding" "binding" {
-  parent    = "//compute.googleapis.com/projects/bu1-prod-app/zones/us-central1-a/instances/${google_compute_instance.proxy_1.name}"
+  parent    = "//compute.googleapis.com/projects/bu1-prod-app/global/instances/${google_compute_instance.proxy_1.name}"
   tag_value = "tagValues/${google_tags_tag_value.web.name}"
 }
+
