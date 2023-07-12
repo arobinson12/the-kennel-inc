ae53a173225fd5700c973e19941e5105386d0b75
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2023-07-12 11:19:43 -0500

    123

--- providers.tf
+++ providers.tf
@@ -1,6 +1,21 @@
+# terraform {
+#   required_version = ">= 0.12"
+# }
+
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.1.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.32.0" # tftest
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.32.0" # tftest
+    }
+  }
 }
+
 #provider "google" {
 #  credentials = file(var.gcp_auth_file)
 #  project     = var.gcp_project
