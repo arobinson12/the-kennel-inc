a85b0fccb809c9273f64e51a3764fee5376c9440
arobinson12 <50377825+arobinson12@users.noreply.github.com>
2023-07-12 15:24:10 -0500

    update5

--- labs/sf-poc/security-foundation-solution/Infra-mod.tf
+++ labs/sf-poc/security-foundation-solution/Infra-mod.tf
@@ -18,12 +18,29 @@
 ## NOTE: This provides PoC demo environment for various use cases ##
 ##  This is not built for production workload ##
 
-provider "google" {
-  #credentials = file(var.gcp_auth_file)
-  project = "interstellar-14"
-  region  = "us-central1"
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.32.0" # tftest
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.32.0" # tftest
+    }
+  }
 }
-#
+
+provider "google" {
+    alias = "service"
+    impersonate_service_account = google_service_account.terraform_service_account.email
+}
+
+provider "google" {
+ 
+}
+#######
 
 # Random id for naming
 resource "random_string" "id" {
--- providers.tf
+++ providers.tf
@@ -1,21 +1,6 @@
 terraform {
   required_version = ">= 0.12"
 }
-
-# terraform {
-#   required_version = ">= 1.1.0"
-#   required_providers {
-#     google = {
-#       source  = "hashicorp/google"
-#       version = ">= 4.32.0" # tftest
-#     }
-#     google-beta = {
-#       source  = "hashicorp/google-beta"
-#       version = ">= 4.32.0" # tftest
-#     }
-#   }
-# }
-
 #provider "google" {
 #  credentials = file(var.gcp_auth_file)
 #  project     = var.gcp_project
@@ -27,8 +12,6 @@ provider "google" {
   project = "interstellar-14"
   region  = "us-central1"
   zone    = "us-central1-c"
-  alias = "service"
-    impersonate_service_account = google_service_account.terraform_service_account.email
 }
 
 provider "kubernetes" {
